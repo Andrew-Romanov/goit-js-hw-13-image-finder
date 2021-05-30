c3d86ebc7908020fed9b4d5a93b8d13724a12443
Andrew-Romanov <andrewromanov@yahoo.com>
2021-05-30 13:07:15 +0300

    Image Finder created

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
 import './sass/main.scss';
+import apiService from './js/apiService';
+import imageCardMarkup from './templates/image-card';
+
+const searchFormEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+const loadMoreButtonEl = document.querySelector('.load-more-button');
+const scrollMarkerEl = document.getElementById('scroll-marker');
+
+function makeFirstSearch(event) {
+  event.preventDefault();
+  event.currentTarget.elements.query.value = event.currentTarget.elements.query.value.trim();;
+
+  apiService.searchQuery = event.currentTarget.elements.query.value;
+  apiService.resetPageNumber();
+
+  clearImageCards();
+  if (apiService.searchQuery === '') {
+    loadMoreButtonEl.classList.add('visually-hidden');
+    return;
+  };
+
+  makeAnotherSearch();
+};
+
+function makeAnotherSearch() {
+  apiService.fetchImages().then(searchResult => {
+
+    if (searchResult.length === 0) {
+      loadMoreButtonEl.classList.add('visually-hidden');
+      return;
+    };
+
+    renderImageCards(searchResult)
+    
+    loadMoreButtonEl.classList.remove('visually-hidden');
+    
+    scrollMarkerEl.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
+    });
+  });
+};
+
+function renderImageCards(images) {
+  galleryEl.insertAdjacentHTML('beforeend', imageCardMarkup(images));
+};
+
+function clearImageCards() {
+  galleryEl.innerHTML = '';
+}
+
+searchFormEl.addEventListener('submit', makeFirstSearch);
+loadMoreButtonEl.addEventListener('click', makeAnotherSearch);
